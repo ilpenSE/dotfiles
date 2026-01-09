773c1263b206511445bc45173df7b9072857602c
ilpenSE <lastplayer8945@gmail.com>
2026-01-09 21:25:52 +0300

    Added magit into emacs init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,10 +6,13 @@
 
 ; gnu packages
 (setq package-archives
-      '(("gnu"   . "https://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")))
+      '(("melpa" . "https://melpa.org/packages/")
+        ("gnu"   . "https://elpa.gnu.org/packages/")
+        ("org"   . "https://orgmode.org/elpa/")))
 
 (package-initialize)
+
+;;; =========== PACKAGES ==========
 
 ;;; theme package
 (add-to-list 'load-path "~/.emacs.d/lisp")
@@ -140,3 +143,15 @@
 
 (global-set-key (kbd "M-<up>") 'move-line-up)
 (global-set-key (kbd "M-<down>") 'move-line-down)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
